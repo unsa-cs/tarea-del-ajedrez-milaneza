cc801c5a2063d37598598ccf523b07cdfb588132
Cristhianepcc <ctaipe@unsa.edu.pe>
2024-10-05 17:04:04 -0500

    Ejercicio 3, aplicar reverse a columna_square , almacenarlo en reverse_square

--- ejercicio3.c
+++ ejercicio3.c
@@ -6,7 +6,7 @@ void display(){
   char** reverse_square = reverse(whiteSquare);
   char** up_square = up(reverse_square,square);
   char** columna_square = repeatV(up_square,2);
-  char** tablero_square = repeatH(columna_square,8);
-  interpreter(tablero_square);
+  char** reverse_columna = reverse(columna_square);
+  interpreter(reverse_columna);
 
 }

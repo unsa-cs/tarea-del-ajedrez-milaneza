72c6a84cf79512e96c8952290632ddea250eddc4
Cristhianepcc <ctaipe@unsa.edu.pe>
2024-10-05 16:56:55 -0500

    Ejercicio 3, aplicar repeatV para up_square, y guardalo en columna_square. Nota: Se genero una columna

--- ejercicio3.c
+++ ejercicio3.c
@@ -5,5 +5,7 @@ void display(){
   char** square = whiteSquare;
   char** reverse_square = reverse(whiteSquare);
   char** up_square = up(reverse_square,square);
-  interpreter(up_square);
+  char** columna_square = repeatV(up_square,2);
+  interpreter(columna_square);
+
 }

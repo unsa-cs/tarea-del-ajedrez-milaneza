e0d98999895f3533d14697f2d996a097a35f6cf4
Paolo-Daniel-B-A <pbenaventea@unsa.edu.pe>
2024-10-05 00:00:00 -0500

    Prueba ejercicio 2, se aplico reverse sobre el codigo del ejercicio 1, se invirtio el color de los cuadrados

---
+++ ejercicio2.c
@@ -0,0 +1,10 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+  char** square1 = whiteSquare;
+  char** square2 = reverse(whiteSquare);
+  char** square3 = join(square1,square2);
+  char** square4 = repeatH(square3,4);
+  interpreter(reverse(square4));
+}

f344444148250c9e69416c2e2a74496bf59dfd3a
Cristhianepcc <ctaipe@unsa.edu.pe>
2024-10-05 17:06:23 -0500

    Ejercicio 3, se aplico join a columna_square y reverse_columna, y se almaceno en patron_square. Nota: se genero el patron del tablero

--- ejercicio3.c
+++ ejercicio3.c
@@ -7,6 +7,7 @@ void display(){
   char** up_square = up(reverse_square,square);
   char** columna_square = repeatV(up_square,2);
   char** reverse_columna = reverse(columna_square);
-  interpreter(reverse_columna);
+  char** patron_square = join(columna_square,reverse_columna);
+  interpreter(patron_square);
 
 }

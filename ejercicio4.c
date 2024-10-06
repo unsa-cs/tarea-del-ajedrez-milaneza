595df0e1c136d643b15b4bb4f8488bf96fc8114c
Alexander Huayhua <ahuayhuap@unsa.edu.pe>
2024-10-06 18:13:19 -0500

    Ejercicio 4: Cambiamos de enfoque, se creo el patron del tablero usando join

--- ejercicio4.c
+++ ejercicio4.c
@@ -4,7 +4,6 @@
 void display(){
   char** squareW = whiteSquare;
   char** squareB = reverse(whiteSquare);
-  char** rookW = superImpose(rook, squareW);
-  char** rookB = superImpose(rook, squareB);
-  interpreter(reverse(rookB));
+  char** pattern = join(squareB, squareW);
+  interpreter(pattern);
 }

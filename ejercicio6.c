f8888be1d31ee5f500ed416f85915ee3957a6b9d
Cristhianepcc <ctaipe@unsa.edu.pe>
2024-10-06 18:49:26 -0500

    Ejercicio 6, se creo un variable tablero que guarda el patron del tablero

---
+++ ejercicio6.c
@@ -0,0 +1,24 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+  char** squareW = whiteSquare;
+  char** squareB = reverse(whiteSquare);
+  char** pattern = join(squareB, squareW);
+  char** board =repeatH(pattern, 4);
+  char** board_reverse = reverse(board);
+  char** patron_tablero = up(board,board_reverse);
+
+  //fichas
+  char** fichas = join(rook,knight);
+  fichas = join(fichas,bishop);
+  fichas = join(fichas,queen);
+  fichas = join(fichas,king);
+  fichas = join(fichas,bishop);
+  fichas = join(fichas,knight);
+  fichas = join(fichas,rook);
+  char** fichas_reverse=reverse(fichas);
+  char** final_board = superImpose(fichas_reverse,board);
+  
+  interpreter(patron_tablero);
+}
